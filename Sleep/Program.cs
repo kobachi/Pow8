5a4d73a3a28ff73bf0e7315057e3c95179f10879
kobachi <kobachi@outlook.com>
2014-03-10 21:55:55 +0900

    Add Hibernate and Sleep

---
+++ Sleep/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sleep {
+	static class Program {
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args) {
+			var options = args.Where(a => !string.IsNullOrEmpty(a) && 2 < a.Length && (a.StartsWith("-") || a.StartsWith("/")));
+			bool force = options.Count(a => a.Substring(1).ToLower() == "force") > 0;
+			bool noWakeup = options.Count(a => a.Substring(1).ToLower() == "nowakeup") > 0;
+			Application.SetSuspendState(PowerState.Suspend, force, noWakeup);
+		}
+	}
+}
