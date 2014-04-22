ccaad5d23936b4b27d64ca787dfd21f6780f92ce
kobachi <kobachi@outlook.com>
2014-04-22 23:30:26 +0900

    Update copyright

--- Hibernate/Properties/AssemblyInfo.cs
+++ Hibernate/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Hibernate")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © kobachi 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
